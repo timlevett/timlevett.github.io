92cfc022aaf00edc0a674a953e4b648ce5e69e62
Tim Levett <levett@wisc.edu>
2015-11-15 11:52:18 -0600

    Make posts modular, add first real post about encapslation.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 (function(){
 
-  var timHome = angular.module('timHome', ['ngMaterial','ui.router']);
-  
+  var timHome = angular.module('timHome', ['ngMaterial','ngSanitize','ui.router']);
+
   timHome.config(function($mdThemingProvider, $stateProvider, $urlRouterProvider) {
     $mdThemingProvider.theme('default')
       .primaryPalette('green')
       .accentPalette('blue');
-      
-  
-    $urlRouterProvider.otherwise("/");  
+
+
+    $urlRouterProvider.otherwise("/");
     $stateProvider
       .state('home', { url: '/', templateUrl : "partials/home.html"});
   });
@@ -20,6 +20,26 @@
         $scope.nav = result.data;
       }, function(){console.warn('issue getting nav')});
   });
-  
+
+  timHome.controller('HomeController',function ($sce, $scope, $http) {
+    $scope.posts = [];
+    $http.get('/json/posts.json', { cache : true})
+      .then(function(result){
+        $scope.posts = result.data;
+      }, function(){console.warn('issue getting posts')});
+  });
+
+  timHome.directive('blogPost', function(){
+    return {
+      restrict : 'E',
+      scope : {
+        title : '@',
+        body : '@',
+        date : '@'
+      },
+      templateUrl : 'partials/blogpost.html'
+    }
+  });
+
   timHome.value('date',new Date());
 })();
