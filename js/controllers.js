ca0f5f092bde183c317c0ee8be9fc0d2f46fa9fe
Tim Levett <tim.levett@gmail.com>
2016-03-06 19:20:30 -0600

    cleanup home

--- js/controllers.js
+++ js/controllers.js
@@ -30,36 +30,40 @@ angular.module('timHome.controllers',[])
   })
   })
   .controller('HomeController',function ($scope, $filter, PostService, filterFilter) {
-  $scope.posts = [];
-  $scope.height = 90;
-  $scope.postid = $scope.$stateParams.postid;
-  $scope.loading=true;
-  PostService.getAllPosts()
-    .then(function(result){
-      $scope.loading = false;
-      $scope.posts = result;
-      if($scope.postid) {
-        for(var index in $scope.posts) {
-          var value = $scope.posts[index];
-          if(value) {
-            if(value.title === $scope.postid) {//exact match (legacy)
-              $scope.post = value;
-              break;
-            } else if(value.id == $scope.postid) { //based on id
-              $scope.post = value;
-              break;
-            } else if(value.title && value.title.replaceAll(" ","_").toLowerCase() === $scope.postid.toLowerCase()) { //perdy url
-              $scope.post = value;
-              break;
+    $scope.posts = [];
+    $scope.lim=7;
+    $scope.height = 90;
+    $scope.postid = $scope.$stateParams.postid;
+    $scope.loading=true;
+    $scope.upLim = function(){
+      $scope.lim = $scope.lim+5;
+    };
+    PostService.getAllPosts()
+      .then(function(result){
+        $scope.loading = false;
+        $scope.posts = result;
+        if($scope.postid) {
+          for(var index in $scope.posts) {
+            var value = $scope.posts[index];
+            if(value) {
+              if(value.title === $scope.postid) {//exact match (legacy)
+                $scope.post = value;
+                break;
+              } else if(value.id == $scope.postid) { //based on id
+                $scope.post = value;
+                break;
+              } else if(value.title && value.title.replaceAll(" ","_").toLowerCase() === $scope.postid.toLowerCase()) { //perdy url
+                $scope.post = value;
+                break;
+              }
             }
           }
+          if(!$scope.post) {
+            $scope.post = {title : '404 : not found', body: 'The page you are looking for does not exist, please try <a href="#/">going home</a>.', date: '01-01-1970'};
+          }
+          $scope.tags = $scope.post.tags;
+          $scope.crumbs = [{title: 'Post'}, {title: $scope.post.date}];
         }
-        if(!$scope.post) {
-          $scope.post = {title : '404 : not found', body: 'The page you are looking for does not exist, please try <a href="#/">going home</a>.', date: '01-01-1970'};
-        }
-        $scope.tags = $scope.post.tags;
-        $scope.crumbs = [{title: 'Post'}, {title: $scope.post.date}];
-      }
     });
   })
   .controller('TagHomeController', function(PostService, $scope){
